5533a78bd046520aa00e152251715538d0cf7be6
Huling <2213470749@qq.com>
2019-12-29 23:21:25 +0800

    12/29 侧边栏

--- blog/urls.py
+++ blog/urls.py
@@ -4,16 +4,19 @@ from . import views
 app_name = 'blog'  # 命名空间 和 url名称的用途
 
 urlpatterns = [
-    # 1.请求参数(无)      => 帖子列表(所有)
+    # 1.1 请求参数()      => 帖子列表(所有)
     path('', views.post_list, name='post_list'),
 
-    # 2.请求参数(str:tag) => 帖子列表(拥有特定tag)
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # 1.2 请求参数(str:tag) => 帖子列表(tag)
+    path('tag/<slug:tag_slug>/', views.post_list_by_tag, name='post_list_by_tag'),
 
-    # 3.请求参数(/tag) => 标签列表(所有)
+    # 1.3 请求参数(str:date) => 帖子列表(date)
+    path('<int:year>/<int:month>', views.post_list_by_date, name='post_list_by_date'),
+
+    # 2.请求参数(/tag) => 标签列表(所有)
     path('tag', views.tag_list, name='tag_list'),
 
-    # 4.请求参数(int:年月日) => 帖子详情
+    # 3.请求参数(int:年月日&标题) => 帖子详情
         # 注意：具有唯一标识性
     path('<int:year>/<int:month>/<int:day>/<str:title>',
          views.post_detail,
--- blog/views.py
+++ blog/views.py
@@ -1,63 +1,177 @@
 from django.shortcuts import render  # 快捷方式：渲染
 from django.shortcuts import get_object_or_404  # 快捷方式：查询对象
 
-
-# 相关模型
-from .models import Post
-from taggit.models import Tag  # 该app中的Tag模型(已经有了？√app中内置的)
-
 # 分页相关
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
 
+# 相关模型
+from .models import Post
+from taggit.models import Tag  # 该app中的内置Tag模型
+
+from markdown import markdown
+from datetime import datetime
+
+
+# 定义一些函数
+def paginate(objs, res_parm, num=7):
+    '''
+    封装分页机制;  
+        参数: 对象集合, 请求参数, 每页数量 ;
+        返回: 页面对象
+    '''
+    paginator = Paginator(objs, num)  # 实例化分页器
+    page = res_parm.get('page')  # 从get请求中获取页数参数
+    Pageobj = paginator.get_page(page)  # 分页器获取  页面对象
+    return Pageobj
+
+def get_page_range(Pageobj):
+    '''
+    获取页数的范围，用于展现的优化; 
+        参数: 页面对象
+        返回: 数量范围的列表
+    '''
+     # 3.优化显示 
+    current_page = Pageobj.number;  # 当前页数
+    max_page = Pageobj.paginator.num_pages;  # 总页数 
+    
+    page_range = list(range(max(current_page-2,1),min(current_page+2,max_page)+1))
+    return page_range
+
+def get_tags_with_count(tags):
+    '''
+    获取各个标签对应的数量;
+        参数: Tag对象集合
+        返回: Tag对象组成的列表，列表以数量逆序
+    '''
+    # ★获取标签对应帖子的数量(猴子补丁)
+    for tag in tags:    # 注意  管理器的运用！
+        tag.posts_count = Post.published.filter(tags=tag).count()
+
+    # 按标签 对应帖子数量 排序
+    tag_list = list(all_tags)   
+    tags = sorted(tag_list, key=lambda x: x.posts_count, reverse=True)
+    return tags
+
+def get_date_with_count(posts):
+    '''
+    获取各个日期对应的数量;
+        参数: 帖子对象集合
+        返回: 字典 键为日期对象，值为对应的数量;
+    '''
+    all_date = {}
+    for post in posts:
+        # 获取 该帖子的时间
+        y =  post.publish.year
+        m = post.publish.month
+
+        # 创建 时间字符串 
+        # date = "%s年%s月"%(y,m)
+        
+        # 也可以创建 日期对象
+        date_obj = datetime(y,m,1);
+        # 获取 该时间的帖子数量    publish.year不可以吗？？
+        
+        all_date[date_obj] = Post.published.filter(publish__year=y,
+                                                    publish__month=m).count()
+    return all_date
+
+def prettify_md(posts):
+    '''
+    美化markdown:
+        参数:  对象
+        返回:  无
+    '''
+    for post in posts:
+        post.body = markdown(post.body, 
+                             extensions=[
+                                'markdown.extensions.extra',
+                                'markdown.extensions.codehilite',
+                                'markdown.extensions.toc',
+                            ])
+
+# 定义一些变量
 all_tags = Tag.objects.all()  # 所有标签
 all_posts = Post.published.all()  # 所有帖子
 
+tags = get_tags_with_count(all_tags)
+all_date = get_date_with_count(all_posts)
 
-# 1. 帖子列表
-def post_list(request, tag_slug=None):
-    
-    # 1.添加标签机制(如果有参数tag)
-    tag = None
-    
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-                        # 注意tag_in!!s ———— Post管理器；
-                        # tags__in = [tag] !!？？
-        posts = all_posts.filter(tags__in=[tag])  
-    else:
-        posts = all_posts
 
-    # 2.添加分页机制
-    paginator = Paginator(posts, 6)  # 实例化分页器
-    page = request.GET.get('page')  # 从get请求中获取页数参数
-    posts = paginator.get_page(page)  # 分页器获取  请求页内容
-
-    # 3.优化显示 
-    current_page = posts.number;  # 当前页数
-    max_page = paginator.num_pages;  # 总页数 
+# 1.1 帖子列表(all)
+def post_list(request):
     
-    page_range = list(range(max(current_page-2,1),min(current_page+2,max_page)+1))
+    # 分页
+    posts = paginate(all_posts, request.GET)
+    # 获取页数范围
+    page_range = get_page_range(posts)
 
-    # 4.构建对象，保存其他需要显示的零散信息(如 页面的标题)
-    # 以里两个文件来显示
-    if tag:
-        template_name = "post_list_by_tag.html"
-    else:
-        template_name = "post_list.html"
-    
-    # 侧边栏的 所有标签和日期
-    tags = get_count_by_tag(all_tags)
-    all_date = get_count_by_date(all_posts)
+    prettify_md(posts)
+    return render(request,
+                  'blog/post_list.html',
+                  { 
+                    # 列表信息
+                   'posts': posts,  
+                   'page_range': page_range,  
+                    # 侧边栏信息    
+                   'tags': tags,
+                   "all_date": all_date,
+                   }) 
+
+# 1.2 帖子列表(tag)
+def post_list_by_tag(request, tag_slug):
+
+    # 获取特定内容
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = all_posts.filter(tags__in=[tag]) 
+        # tags ———— Post管理器；# tags__in = [tag] 
+
+    # 分页
+    posts = paginate(posts, request.GET)
+    # 获取页面范围
+    page_range = get_page_range(posts)
+    prettify_md(posts)
 
     return render(request,
-                  'blog/' + template_name,
-                  {'posts': posts,  # posts为页面对象(一页)
-                   'page_range':page_range,  # 分页渲染的页数
-                   'tag': tag,
-                   'all_tags':all_tags,
+                  "blog/post_list_by_tag.html",
+                  {
+                    # 标题信息
+                   'tag':tag,
+                    # 列表信息
+                   'posts':posts,
+                   'page_range':page_range,
+                    # 侧边栏信息    
+                   'tags':tags,
                    "all_date":all_date,
-                   }) 
+                  })
+
+# 1.3 帖子列表(date)
+def post_list_by_date(request, year, month):
+
+    # 获取特定内容
+    posts = all_posts.filter(publish__year=year, publish__month=month)
+
+    # 分页
+    posts = paginate(posts, request.GET)
+    # 获取页面范围
+    page_range = get_page_range(posts)
+    prettify_md(posts)
+
+    date = str(year) + "年" + str(month) + "月"
+
+    return render(request,
+                  "blog/post_list_by_date.html",
+                  {
+                    # 标题信息
+                   'date':date,
+                    # 列表信息
+                   'posts':posts,
+                   'page_range':page_range,
+                    # 侧边栏信息    
+                   'tags':tags,
+                   "all_date":all_date,
+                  })
+
 
 # 2. 帖子详情
 def post_detail(request, year, month, day, title):
@@ -75,7 +189,11 @@ def post_detail(request, year, month, day, title):
                              publish__day=day,
                              title=title,
                              )
-    
+    post.body = markdown(post.body, extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
 
     page = posts_list.index(post)+1  # 当前页
 
@@ -90,53 +208,11 @@ def post_detail(request, year, month, day, title):
                    })
 
 
-# 3.分类列表
+# 4.分类列表
 def tag_list(request,):
 
-    tags = get_count_by_tag(all_tags)
+    tags = get_tags_with_count(all_tags)
     return render(request,
                   "blog/tag.html",
                   {'tags':tags,
                    })
-
-
-
-def get_count_by_tag(tags):
-    '''
-    获取各个标签对应的数量，返回Tag对象组成的列表，列表以数量逆序
-    '''
-    # ★获取标签对应帖子的数量(猴子补丁)
-    for tag in tags:    # 注意  管理器的运用！
-        tag.posts_count = Post.published.filter(tags=tag).count()
-
-    # 按标签 对应帖子数量 排序
-    tag_list = list(all_tags)   
-    tags = sorted(tag_list, key=lambda x: x.posts_count, reverse=True)
-    return tags
-
-
-def get_count_by_date(posts):
-    '''
-    获取各个日期对应的数量，返回字典:键类型为str型，值为int型
-    '''
-    all_date = {}
-    for post in posts:
-        # 获取 该帖子的时间
-        y =  post.publish.year
-        m = post.publish.month
-        # 创建 时间字符串 
-        date = "%s年%s月"%(y,m)
-        # 获取 该时间的帖子数量    publish.year不可以吗？？
-        count = Post.published.filter(publish__year=y,
-                                      publish__month=m).count()
-        all_date[date] = count;
-    return all_date
-
-
-
-''' 
-以前被css样式所迫 --
-    point = (len(tags)+1)//2  #
-    tags_left = tags[:point]  # 因为版面原因，将数据分成2部分
-    tags_right = tags[point:]  # 因为切片为视图，即复制原有对象，故将此操作移到
-'''
