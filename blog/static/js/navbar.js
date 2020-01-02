8371facd31649b07ba6e1503cdc0269018aded74
Huling <2213470749@qq.com>
2020-01-02 16:00:36 +0800

    1/2 响应式

--- blog/static/js/navbar.js
+++ blog/static/js/navbar.js
@@ -1,11 +1,13 @@
 
 
 $(function () {
+
+    // 导航栏的向上slidey隐藏
     var beforePos = 0;
 
     $(window).scroll(function () {
         var nowPos = $(window).scrollTop();
-        console.log(nowPos)
+
         if (nowPos > beforePos) {
             $(".navbar").addClass("navbar-slide-up");
         } else {
@@ -14,4 +16,14 @@ $(function () {
         beforePos = nowPos;
     })
 
+    // 导航栏  透明的背景
+    $(window).scroll(function () {
+        $(".navbar").toggleClass("navbar-transparent",
+        $(window).scrollTop() == 0);
+    })
+
+    // 导航栏的媒体查询
+    $(".navbar-toggle").click(function() {
+        $(".navbar-collapse").toggle(300);
+    })
 })
