cd7ee4ca012fde9f93dd09cf75a5c4b1b63614da
hlf <2213470749@qq.com>
2020-11-23 19:20:31 +0800

    1

---
+++ StaticComeHere/js/navbar.js
@@ -0,0 +1,31 @@
+
+
+$(function () {
+
+    // 导航栏的向上slidey隐藏
+    var beforePos = 0;
+
+    $(window).scroll(function () {
+        var nowPos = $(window).scrollTop();
+
+        if (nowPos > beforePos) {
+            $(".navbar").addClass("navbar-slide-up");
+        } else {
+            $(".navbar").removeClass("navbar-slide-up");
+        }
+        beforePos = nowPos;
+    })
+
+    // 导航栏  透明的背景
+    $(window).scroll(function () {
+        $(".navbar").toggleClass("navbar-transparent",
+        $(window).scrollTop() == 0);
+    })
+
+    // 导航栏的媒体查询
+    $(".navbar-toggle").click(function() {
+        // jquery的预设动画
+        $(".navbar-collapse").slideToggle(400);
+        
+    })
+})
