8a17658d305e9cb46b9643f28be1250f927f92eb
hlf <2213470749@qq.com>
2020-11-25 10:56:15 +0800

    1

--- blog/static/js/navbar.js
+++ blog/static/js/navbar.js
@@ -26,6 +26,12 @@ $(function () {
     $(".navbar-toggle").click(function() {
         // jquery的预设动画
         $(".navbar-collapse").slideToggle(400);
+
+
+    // "我的"菜单的toggle(手机)
+    $("#my").click(function() {
+        $("#my-sub").toggleClass("show")
+    })
         
     })
 })
